ad3476399a5cc445cb5cdad00b73c5324ffeaf97
XPukar <80558146+XPukar@users.noreply.github.com>
2025-05-27 14:01:04 +1000

    Add files via upload
    Entry

---
+++ logo.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Logo = void 0;
+var _react = _interopRequireDefault(require("react"));
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+const Logo = () => /*#__PURE__*/_react.default.createElement("svg", {
+  xmlns: "http://www.w3.org/2000/svg",
+  width: "32",
+  height: "32",
+  viewBox: "0 0 32 32"
+}, /*#__PURE__*/_react.default.createElement("g", {
+  fill: "none"
+}, /*#__PURE__*/_react.default.createElement("path", {
+  fill: "#FDD009",
+  d: "M610 1580 l0 -160 59 0 c66 0 62 -8 76 121 4 31 11 62 16 70 10 16 159 25 245 14 l54 -6 -2 -482 -3 -482 -82 -29 c-82 -28 -83 -29 -83 -62 l0 -34 265 0 265 0 0 34 c0 33 -1 34 -82 62 l-83 29 -3 483 -2 482 78 0 c44 0 106 3 138 6 83 8 91 0 105 -113 l13 -93 58 0 58 0 0 160 0 160 -545 0 -545 0 0 -160z"
+})));
+exports.Logo = Logo;
